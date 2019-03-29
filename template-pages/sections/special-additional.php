4bd83345ef34ad16b190af0dc078569041a85a6d
brilik <megabrilik@gmail.com>
2019-03-29 13:19:00 +0200

    Fix section

--- template-pages/sections/special-additional.php
+++ template-pages/sections/special-additional.php
@@ -3,7 +3,19 @@
 		<h3 class="title"><?=get_sub_field('title');?></h3>
 		<div class="special-box">
 			<div class="special-wrap">
-				<?php $left = get_sub_field('left');
+                <?php
+
+                $left = get_sub_field('left');
+                $right = get_sub_field('right');
+
+                if( $right && get_sub_field('img_left') ): ?>
+                    <div class="special-wrap__photo special-wrap__photo_ml">
+                        <img class="js-img-lazy" data-src="<?=$right['img']['url'];?>" src="/" alt="<?=$right['img']['alt'];?>">
+                    </div>
+                <?php
+
+                endif;
+
 				if( $left ): ?>
 				<div class="special-wrap__text">
 					<?php echo $left['desc'];
@@ -13,9 +25,9 @@
 					</a>
 					<?php endif; ?>
 				</div>
-				<?php endif;
-				$right = get_sub_field('right');
-				if( $right ): ?>
+				<?php
+                endif;
+				if( $right && !get_sub_field('img_left') ): ?>
 				<div class="special-wrap__photo special-wrap__photo_ml">
 					<img class="js-img-lazy" data-src="<?=$right['img']['url'];?>" src="/" alt="<?=$right['img']['alt'];?>">
 				</div>
